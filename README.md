81983a44f9b7dea95fa046979575ce80724c4f6e
yangyumei <305883594@qq.com>
2018-07-04 11:44:51 +0800

    :art:

---
+++ README.md
@@ -0,0 +1,19 @@
+# vue-cli多页面应用
+> 基于vue脚手架的多页面打包
+
+### 杨玉梅 2018/07/04
+
+## Build Setup
+
+``` bash
+# install dependencies
+npm install
+
+# serve with hot reload at localhost:8080
+npm run dev
+
+# build for production with minification
+npm run build
+## 如果新增了页面,需要重新打包一次,页面打包成功之后,才能在浏览器中访问到
+```
+# vue-cli-multipage
---
+++ src/ykbb/user_medecin_shop/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <div>用户-药铺</div>
+  </div>
+</template>
+
+<script>
+  import 'common/css/reset.css';
+  export default {
+    name: 'app',
+  }
+</script>
+
+<style>
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
+</style>
